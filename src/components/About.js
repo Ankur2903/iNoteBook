daca64f4c4da9e01021d16405fde5d7031756a30
Ankur2903 <sainiankur0508@gmail.com>
2025-01-23 11:54:40 +0530

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image1 from './Screenshot 2025-01-23 114854.png'
 
 const About = () => {
   return (
     <div>
-      this is about
+      <h2>iNoteBook</h2>
+      <img src = {Image1} style={{width: "84vw"}}></img>
     </div>
   )
 }
